933293d5c4b76b68977971f6fb0a5de6870e8b9c
enzoiannini96 <enzo.iannini96@gmail.com>
2021-06-14 18:03:41 +0200

    [IanniniVI] fix cookie federa name

--- src/main/java/com/nttdata/mock/mms/api/utils/Constants.java
+++ src/main/java/com/nttdata/mock/mms/api/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final int SUCCESS_CODE = 0;
 	public static final String DEFAULT_HTTP_500_MESSAGE = "Internal Server error! If the problem persists, please contact system administrator!";
 	public static final int MAX_SIZE_LIST_IN_CLAUSE = 900;
-	public static final String COOKIENAME = "authFEDERA";
+	public static final String COOKIENAME = "AuthFEDERA";
 
 	private Constants() {
 
