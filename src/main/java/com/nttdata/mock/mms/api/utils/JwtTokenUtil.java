64975d59d135b6aea59b005916594256f378945e
hermitf4 <silente4f@gmail.com>
2021-06-15 00:50:07 +0200

    [FazioDa] Integration Algorithm RSA256

--- src/main/java/com/nttdata/mock/mms/api/utils/JwtTokenUtil.java
+++ src/main/java/com/nttdata/mock/mms/api/utils/JwtTokenUtil.java
@@ -1,12 +1,22 @@
 package com.nttdata.mock.mms.api.utils;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.FileCopyUtils;
@@ -19,92 +29,116 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.nttdata.mock.mms.api.exceptions.MockMmmsException;
 
-public class JwtTokenUtil implements Serializable{
+public class JwtTokenUtil implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60 * 1000;
-	
+
 	private String token = null;
-	
+
 	public JwtTokenUtil(String jwtToken) {
 		this.token = jwtToken;
 	}
-	
-	
+
 	public JwtTokenUtil() {
 		super();
 	}
 
-
 	public String getToken() {
-	    return token;
+		return token;
 	}
-	
-	public JwtTokenUtil generateToken(String tokenFEDERA) throws IllegalArgumentException, JWTCreationException, MockMmmsException {
-		Map<String, Object> claims = new HashMap<String, Object>(); 
-		
+
+	public JwtTokenUtil generateToken(String tokenFEDERA)
+			throws IllegalArgumentException, JWTCreationException, MockMmmsException {
+		Map<String, Object> claims = new HashMap<String, Object>();
+
 		String subJect = "mock-mms-api";
-		
+
 		claims.put("CODICEFISCALE", tokenFEDERA);
-		
+
 		return doGenerateToken(claims, subJect);
 	}
 
-	
 	private static JwtTokenUtil doGenerateToken(Map<String, Object> claims, String subject) throws IllegalArgumentException, JWTCreationException, MockMmmsException {
 		Instant issuedAt = Instant.now();
-		
+
 		Builder builder = JWT.create().withSubject(subject).withIssuedAt(Date.from(issuedAt)).withExpiresAt(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY));
 
 		claims.forEach((claimName, value) -> builder.withClaim(claimName, StringUtils.join(value)));
 
-		return new JwtTokenUtil(builder.sign(Algorithm.HMAC256(readJwtSecretKeyFromFile())));
+		return new JwtTokenUtil(builder.sign(Algorithm.RSA256((RSAPublicKey) getPublicKey(), ((RSAPrivateKey) getPrivateKey()))));
 	}
 
-	
 	public DecodedJWT validateToken(String token) throws MockMmmsException {
-		JWTVerifier verifiefToken = JWT.require(Algorithm.HMAC256(readJwtSecretKeyFromFile())).build();
-		
+
+		JWTVerifier verifiefToken = JWT.require(Algorithm.RSA256((RSAPublicKey) getPublicKey(), ((RSAPrivateKey) getPrivateKey()))).build();
+
 		try {
 			return verifiefToken.verify(token);
 		} catch (Exception e) {
 			throw new MockMmmsException(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, e.getMessage());
 		}
 	}
-	
-	public static String readJwtSecretKeyFromFile () throws MockMmmsException{
-		
-		String secretKey = null;
-		
-		try {
-			File file = new File("src/main/resources/private_key");
-			secretKey = new String(FileCopyUtils.copyToByteArray(file));
+
+	private static PrivateKey getPrivateKey() throws MockMmmsException {
+		PrivateKey key = null;
+
+		try (InputStream fis = FileUtils.openInputStream(new File("src/main/resources/private_key"))) {
+			PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(Base64.getMimeDecoder().decode((StringUtils.replaceEachRepeatedly(new String(FileCopyUtils.copyToByteArray(fis)), new String[] { "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----" }, new String[] { "", "" })).trim()));
+
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+
+			key = kf.generatePrivate(spec);
 		} catch (Exception e) {
 			throw new MockMmmsException(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, e.getMessage());
 		}
-		
-		return secretKey;
+
+		return key;
 	}
-	
-	public DecodedJWT decodeJwtTokenFedera(String token) throws MockMmmsException{
-		
-		String publicKey = null;
-		
-		try {
-			File file = new File("src/main/resources/jwt-federa-key");
-			publicKey = new String(FileCopyUtils.copyToByteArray(file));
+
+	private static PublicKey getPublicKey() throws MockMmmsException {
+		PublicKey key = null;
+
+		try (InputStream fis = FileUtils.openInputStream(new File("src/main/resources/public_key"))) {
+
+			X509EncodedKeySpec spec = new X509EncodedKeySpec(Base64.getMimeDecoder().decode((StringUtils.replaceEachRepeatedly(new String(FileCopyUtils.copyToByteArray(fis)), new String[] { "-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----" }, new String[] { "", "" })).trim()));
+
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+
+			key = kf.generatePublic(spec);
 		} catch (Exception e) {
 			throw new MockMmmsException(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, e.getMessage());
 		}
-		
-		JWTVerifier verifiefToken = JWT.require(Algorithm.HMAC256(publicKey)).build();
-		
+
+		return key;
+	}
+
+	private static PublicKey getPublicFederaKey() throws MockMmmsException {
+		PublicKey key = null;
+
+		try (InputStream fis = FileUtils.openInputStream(new File("src/main/resources/jwt-federa-key"))) {
+
+			X509EncodedKeySpec spec = new X509EncodedKeySpec(Base64.getMimeDecoder().decode((StringUtils.replaceEachRepeatedly(new String(FileCopyUtils.copyToByteArray(fis)), new String[] { "-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----" }, new String[] { "", "" })).trim()));
+
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+
+			key = kf.generatePublic(spec);
+		} catch (Exception e) {
+			throw new MockMmmsException(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, e.getMessage());
+		}
+
+		return key;
+	}
+
+	public DecodedJWT decodeJwtTokenFedera(String token) throws MockMmmsException {
+
+		JWTVerifier verifiefToken = JWT.require(Algorithm.RSA256((RSAPublicKey) getPublicFederaKey(), null)).build();
+
 		try {
 			return verifiefToken.verify(token);
 		} catch (Exception e) {
 			throw new MockMmmsException(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, e.getMessage());
 		}
-		
+
 	}
 }
- 
