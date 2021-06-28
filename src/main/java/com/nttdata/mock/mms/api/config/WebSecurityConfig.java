1c003986c0cbbd591b834c44441bec9ff03ad81b
enzoiannini96 <enzo.iannini96@gmail.com>
2021-06-28 11:02:25 +0200

    [IanniniVI] various fix

--- src/main/java/com/nttdata/mock/mms/api/config/WebSecurityConfig.java
+++ src/main/java/com/nttdata/mock/mms/api/config/WebSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.nttdata.mock.mms.api.filters.JwtRequestFilter;
 
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 			.csrf().disable()
-			.addFilterBefore(jwtRequestFilter, BasicAuthenticationFilter.class)
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
 			.authorizeRequests() 
 			.antMatchers("/", 
 					     "/getAuthentication", 
